a27506bebad0487ae5bf1b7213946fb1de2998c4
togami <tuabobo123@gmail.com>
2020-05-24 20:34:50 +0900

    add extension

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
-FROM node:12.16.3-alpine3.10
+FROM node:12.16.3-alpine3.9
 COPY ./front /front
 WORKDIR /front
 RUN npm install
-CMD npm start
 EXPOSE 3000
+ENV CI true
+CMD npm start
--- front/.eslintrc.js
+++ front/.eslintrc.js
@@ -1,37 +1,36 @@
 module.exports = {
   env: {
     browser: true,
-    es6: true
+    es6: true,
   },
   extends: [
-    'plugin:react/recommended',
-    'standard'
+    "plugin:react/recommended",
+    "standard",
+    "plugin:prettier/recommended",
+    "prettier/@typescript-eslint",
   ],
   globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly'
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
   },
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaFeatures: {
-      jsx: true
+      jsx: true,
     },
     ecmaVersion: 2018,
-    sourceType: 'module'
+    sourceType: "module",
   },
-  plugins: [
-    'react',
-    '@typescript-eslint',
-    'jest'
-  ],
+  plugins: ["react", "@typescript-eslint", "jest", "prettier"],
   rules: {
+    "prettier/prettier": "error",
   },
-  "settings": {
+  settings: {
     react: {
-      version: 'detect'
-    }
+      version: "detect",
+    },
   },
-  "env": {
-    "jest/globals": true
-  }
-}
+  env: {
+    "jest/globals": true,
+  },
+};
